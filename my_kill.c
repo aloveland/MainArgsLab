969283c07bbf4a9518691021dba8d5635a130ff3
aloveland <44244521+aloveland@users.noreply.github.com>
2020-09-09 00:02:46 -0400

    Update my_kill.c

--- my_kill.c
+++ my_kill.c
@@ -3,6 +3,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 int main(int argc, char *argv[]) {
   if (argc != 2) {
         printf("Error - command format is $ my_kill pid\n");
